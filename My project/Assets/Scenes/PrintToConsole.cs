1f1c0798b8477dced89ad6553d97ece327d4dccd
mshraboni <mshraboni@ualr.edu>
2024-09-03 09:33:23 -0500

    Doing my First Project called Obstacle Project

---
+++ My project/Assets/Scenes/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm printing to the console");
+        Debug.Log("Hello, I have no doors or windows, who on earth designed me?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
